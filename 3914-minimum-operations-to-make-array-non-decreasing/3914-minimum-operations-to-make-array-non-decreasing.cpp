80e0d81c3f2b7e7b1ba12b5b2aa553bbed89c786
Aniket <150279055+aaniket21@users.noreply.github.com>
2026-08-03 11:20:52 +0530

    Time: 0 ms (100%), Space: 136.7 MB (89.37%) - LeetHub

---
+++ 3914-minimum-operations-to-make-array-non-decreasing/3914-minimum-operations-to-make-array-non-decreasing.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    long long minOperations(vector<int>& nums) {
+        long long ans = 0;
+
+        for (int i = 0; i < nums.size() - 1; i++) {
+            if (nums[i] > nums[i + 1])
+                ans += nums[i] - nums[i + 1];
+        }
+
+        return ans;
+    }
+};
