0acb2896dd4e9efc9f287f4ae05e8f4450ec98b2
Aniket <150279055+aaniket21@users.noreply.github.com>
2026-08-24 12:18:49 +0530

    Time: 3 ms (35.7%), Space: 14.5 MB (16.17%) - LeetHub

---
+++ 2562-find-the-array-concatenation-value/2562-find-the-array-concatenation-value.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    long long findTheArrayConcVal(vector<int>& nums) {
+        long long ans=0;
+        int i=0,j=nums.size()-1;
+
+        while(i<j){
+            string con=to_string(nums[i])+to_string(nums[j]);
+            ans+=stoi(con);
+            i++;
+            j--;
+        }
+
+        if(i==j) ans+=nums[i];
+
+        return ans;
+    }
+};
